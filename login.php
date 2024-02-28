50ec39a3e5d61250a462cfbdcee3e5b04454ee91
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2024-02-28 21:26:32 +0700

    update login layoyt

--- login.php
+++ login.php
@@ -14,14 +14,14 @@
                     <img class="img-in" src="./images/login-background.webp" alt="none">
                 </div>
                 <div class="login-box-form">
-                    <form method="post" action="login_action.php">
-                        <h2>LOGIN FORM</h2><br>
-                        Email<br>
-                        <input type="email" name="email" ><br>
-                        Password<br>
-                        <input type="password" name="password"><br>
-                        <input type="submit" value="LOGIN"><br><br>
-                        Don't have an account yet? <a href="signup.php">Sign up</a>
+                    <form class="form-login" method="post" action="login_action.php">
+                        <div class="header-form"><b>LOGIN FORM</b></div><br>
+                        <div class="email-text">Email:</div>
+                        <input class="email-box" type="email" name="email" >
+                        <div class="password-text">Password:</div>
+                        <input class="password-box" type="password" name="password"><br>
+                        <input class="button-submit" type="submit" value="LOGIN"><br><br>
+                        Don't have an account yet? <a class="signup-button" href="signup.php">Sign up</a>
                     </form>
                 </div>
             </div>
